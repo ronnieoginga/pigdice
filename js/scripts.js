4864e56e8e532f366ecf90cd6c17199bfb8ff171
ronnieoginga <oginga001ronney@gmail.com>
2018-12-17 07:46:06 +0300

    adde turn function to player

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ var player2="";
 var throwdice = function () {
   return Math.floor(Math.random())+1;
 }
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
