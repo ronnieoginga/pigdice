ce08d7b36ae935a860493880ae000ce9a5de657d
ronnieoginga <oginga001ronney@gmail.com>
2018-12-17 08:13:48 +0300

    functinal hold

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,26 @@ function Player(turn) {
   this.turn = turn;
   this.playerName;
 }
+
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
+  } else {
+  this.tempscore += this.roll;
+  }
+}
+
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert(this.playerName + ", your turn is over, pass the mouse!");
+}
+
+Player.prototype.changeturn = function () {
+  if (this.roll ===1) {
+    this.turn = false;
+  } else {
+    this.turn = true;
+  }
+}
